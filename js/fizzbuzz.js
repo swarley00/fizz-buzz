03eb4d48eea65bd3173991e6cf2473659c9082e5
swarley00 <jason.caryl@gmail.com>
2015-04-08 17:40:17 -0400

    Numbers divisible by 3, numbers divisible by 5, and every other number

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,18 +1,14 @@
 $(document).ready(function() {
 	var myBody = $("body");
 	for (var i = 1; i <= 100; i++) {
-		if (i%3 === 0) {
-			console.log("fizz");
+		if ((i%3) === 0) {
+			myBody.append(" fizz" + ", ");
 		}
-		else if (i%5 === 0) {
-			console.log("buzz");
+		else if ((i%5) === 0) {
+			myBody.append(" buzz" + ", ");
 		}
-		// else if (i%5) {
-		// 	console.log("buzz");
-		// 	// myBody.append("buzz");
-		// }
-		// else {
-		// 	console.log(i);
-		// }
+		else {
+			myBody.append(i + ", ");
+		}
 	}
 });
