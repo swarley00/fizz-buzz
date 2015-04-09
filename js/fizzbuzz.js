ecaf1569f8dd9ac4f75e064962cad7c4e38665ec
swarley00 <jason.caryl@gmail.com>
2015-04-09 12:43:57 -0400

    Nailed it, but need to fix the last comma

--- js/fizzbuzz.js
+++ js/fizzbuzz.js
@@ -1,14 +1,14 @@
 $(document).ready(function() {
 	var myBody = $("body");
 	for (var i = 1; i <= 100; i++) {
-		if ((i%3) === 0) {
+		if (((i%3) === 0) && ((i%5) === 0)) {
+			myBody.append(" fizz buzz" + ", ");
+		}
+		else if ((i%3) === 0) {
 			myBody.append(" fizz" + ", ");
 		}
 		else if ((i%5) === 0) {
 			myBody.append(" buzz" + ", ");
-		}
-		else if (((i%3) === 0) && ((i%5) === 0)) {
-			myBody.append(" fizz buzz" + ", ");
 		}
 		else {
 			myBody.append(i + ", ");
